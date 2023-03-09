809837f5baec5dc6261a4cb14d7df6e2f823f79c
amurji22 <amurji22@uwo.ca>
2023-03-09 09:34:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import pygame, time
 
+pygame.init()
+
+bg_img = pygame.image.load('ezgif-1-dfd70dbc93.jpg')
+bg_rect = bg_img.get_rect()
+
+screen = pygame.display.set_mode((1366, 768))
+screen_rect = screen.get_rect()
+
+
+def render():
+    screen.blit(bg_img, bg_rect)
+    pygame.display.flip()
+
+
+render()
+running = True
+# game loop
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    render()
+    time.sleep(0.05)
+pygame.quit()
