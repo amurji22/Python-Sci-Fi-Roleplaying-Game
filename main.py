eafb6cc4a2b847cb15631720d23d8449b46ae0ef
Katja Murray <kmurra84@uwo.ca>
2023-03-13 18:12:36 -0400

    Button Functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import gameElements
 
 pygame.init()
 
@@ -12,12 +13,15 @@ screen_rect = screen.get_rect()
 start_button_img = pygame.image.load('Start.png')
 start_button_rect = start_button_img.get_rect()
 
+start_button = gameElements.Button(screen_rect.centerx, screen_rect.centery, start_button_img, 0.8, 0.9)
+
 
 def render():
     screen.blit(bg_img, bg_rect)
-    screen.blit(start_button_img, (384, 150))
+    start_button.draw(screen)
     pygame.display.flip()
 
+game_state = gameElements.GameState.TITLE
 
 render()
 running = True
@@ -27,6 +31,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if start_button.draw(screen):
+                print('START')
     render()
     time.sleep(0.05)
 pygame.quit()
