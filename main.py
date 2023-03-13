b49d56170e96a338ba636d955c88ef80e83989f5
Kat <90776837+katmurray16@users.noreply.github.com>
2023-03-13 18:23:38 -0400

    Button Functionality Merge Conflicts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import gameElements
 
 pygame.init()
 
@@ -11,6 +12,7 @@ screen_rect = screen.get_rect()
 
 start_button_img = pygame.image.load('Start.png')
 start_button_rect = start_button_img.get_rect()
+start_button = gameElements.Button(screen_rect.centerx, screen_rect.centery, start_button_img, 0.8, 0.9)
 
 exit_button_img = pygame.image.load('Exit.png')
 exit_button_rect = exit_button_img.get_rect()
@@ -18,10 +20,11 @@ exit_button_rect = exit_button_img.get_rect()
 
 def render():
     screen.blit(bg_img, bg_rect)
-    screen.blit(start_button_img, (384, 150))
+    start_button.draw(screen)
     screen.blit(exit_button_img,(875,555))
     pygame.display.flip()
 
+game_state = gameElements.GameState.TITLE
 
 render()
 running = True
@@ -32,6 +35,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
+            if start_button.draw(screen):
+                print('START')
             exit_button_rect = exit_button_img.get_rect().move(875,555)
             if exit_button_rect.collidepoint(event.pos):
                 running = False
