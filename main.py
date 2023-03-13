a7ed1d97f7fd058de1bcd569c6443e89dcb2a8ab
Katja Murray <kmurra84@uwo.ca>
2023-03-13 18:27:36 -0400

    Conflict resolution

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ screen_rect = screen.get_rect()
 
 start_button_img = pygame.image.load('Start.png')
 start_button_rect = start_button_img.get_rect()
-
 start_button = gameElements.Button(screen_rect.centerx, screen_rect.centery, start_button_img, 0.8, 0.9)
+
+exit_button_img = pygame.image.load('Exit.png')
+exit_button_rect = exit_button_img.get_rect()
 
 
 def render():
     screen.blit(bg_img, bg_rect)
     start_button.draw(screen)
+    screen.blit(exit_button_img,(875,555))
     pygame.display.flip()
 
 game_state = gameElements.GameState.TITLE
@@ -34,6 +37,9 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if start_button.draw(screen):
                 print('START')
+            exit_button_rect = exit_button_img.get_rect().move(875,555)
+            if exit_button_rect.collidepoint(event.pos):
+                running = False
     render()
     time.sleep(0.05)
 pygame.quit()
