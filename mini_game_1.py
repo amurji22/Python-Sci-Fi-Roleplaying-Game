822c11bbbc034dbca4f48ea325eed0c720e163be
amurji22 <113624697+amurji22@users.noreply.github.com>
2023-03-20 23:19:07 -0400

    Update mini_game_1.py

--- mini_game_1.py
+++ mini_game_1.py
@@ -14,10 +14,18 @@ body_list = [(x, y)]
 
 clock = pygame.time.Clock()
 
+game_over = False
+
+font = pygame.font.SysFont("bahnschrift", 100)
+
 def snake():
-    global x, y, food_x, food_y
+    global x, y, food_x, food_y, game_over
     x = (delta_x+x)%1200
     y = (delta_y+y)%800
+
+    if((x,y) in body_list):
+        game_over = True
+        return
 
     body_list.append((x, y))
     
@@ -28,6 +36,8 @@ def snake():
         del body_list[0]
 
     screen.fill((0, 0, 0))
+    score = font.render("Score: "+ str(len(body_list)), True, (255, 255, 0))
+    screen.blit(score, [0, 0])
     pygame.draw.rect(screen, (255, 0, 0), [food_x,food_y, 50, 50])
     for (i,j) in body_list:
         pygame.draw.rect(screen, (255, 255, 255), [i, j, 50, 50])
@@ -35,6 +45,13 @@ def snake():
 
 running = True
 while running:
+    if(game_over):
+        screen.fill((0,0,0))
+        msg = font.render("GAME OVER!", True, (255, 255, 255))
+        screen.blit(msg, [1200//3, 800//3])
+        pygame.display.update()
+        time.sleep(10)
+        pygame.quit()
    #Handle the close or x button
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
