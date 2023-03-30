97a6fcd936be8f0f655ebe5a3cdfd13b1a392d50
bgorchin <114183366+bgorchin@users.noreply.github.com>
2023-03-30 10:18:43 -0400

    Add files via upload
    scene for when you are captured by cop

---
+++ if_captured.py
@@ -0,0 +1,42 @@
+import pygame, gameElements
+
+pygame.init()
+
+# create the screen and its rect object
+screen = pygame.display.set_mode((1200, 768))
+screen_rect = screen.get_rect()
+
+# load the background and create its rect
+bg_img = pygame.image.load("if_captured_background.jpg")
+bg_rect = bg_img.get_rect()
+
+# create the button objects
+bribeBtn = gameElements.Button('bribe_button.png',(225, 500), 125, 100, 10, 20, screen)
+fightBtn = gameElements.Button('fight_button.png',(975, 500), 125, 100, 10, 20, screen)
+
+# render function
+def render():
+    screen.blit(bg_img, bg_rect)
+    bribeBtn.check_hover()
+    fightBtn.check_hover()
+
+    pygame.display.flip()
+
+# game loop
+running = True
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if bribeBtn.command == True:
+        exec(open('').read())
+        bribeBtn.command = False
+    elif fightBtn.command == True:
+        exec(open('').read())
+        fightBtn.command = False
+
+    render()
+
+pygame.quit()
