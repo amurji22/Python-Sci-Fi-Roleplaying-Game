60d8cff987c35b93a349bd05912355a9289e177f
mshar224 <mshar224@uwo.ca>
2023-03-30 09:58:36 -0400

    Bribe Scene

---
+++ bribe_scene.py
@@ -0,0 +1,31 @@
+# import pygame
+# # from escape_cops import alien_type
+
+# pygame.init()
+
+# # create screen
+# screen = pygame.display.set_mode((1200, 740))
+
+# # load the background and create its rect
+# bg_img = pygame.image.load("success_background.jpg")
+# bg_rect = bg_img.get_rect()
+
+# # render
+# def render():
+#     screen.blit(bg_img, bg_rect)
+    
+
+#     pygame.display.flip()
+
+# running = True
+# # game loop
+# while running:
+#     # event loop
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+
+#     render()
+
+# pygame.quit()
