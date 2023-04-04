05a1cd56a47ed884503a94503007bc02337e05c9
bgorchin <114183366+bgorchin@users.noreply.github.com>
2023-04-04 16:58:07 -0400

    Update fight_cops.py
    added functionality for power ups depending on alien chosen

--- fight_cops.py
+++ fight_cops.py
@@ -1,5 +1,6 @@
 import pygame, time
 import random
+import gameElements
 
 pygame.init()
 
@@ -46,6 +47,16 @@ def render():
     screen.blit(cop1_img, cop1_rect)
     screen.blit(cop2_img, cop2_rect)
     screen.blit(cop3_img, cop3_rect)
+    
+    if(gameElements.healer_power_up):
+        if hearts_lost == 0:
+            screen.blit(heart_1, heart_1_rect)
+            screen.blit(heart_2, heart_2_rect)
+        elif hearts_lost == 1:
+            screen.blit(heart_1, heart_1_rect)
+    else:
+        if hearts_lost == 1:
+            screen.blit(heart_1, heart_1_rect)
 
     pygame.display.flip()
 
@@ -54,9 +65,31 @@ def moveit_it():
     RED = (255, 0, 0)
     pew_pew.y -= laser_move_y
 
+if(gameElements.healer_power_up):
+    hearts_lost = 0
+else:
+    hearts_lost = 1
+
+heart_1 = pygame.image.load("images/heart.png")
+heart_2 = pygame.image.load("images/heart.png")
+heart_1_rect = heart_1.get_rect()
+heart_2_rect = heart_2.get_rect()
+heart_1_rect.topleft = (20,20)
+heart_2_rect.topleft = (70,20)
+
 cops_passed_counter = 50
 cop_speed = (0, 1)
 cop_x = 2
+
+move_speed_ship_R = (0,0)
+move_speed_ship_L = (0,0)
+
+if (gameElements.speed_power_up):
+    move_speed_ship_L = (2.5,0)
+    move_speed_ship_R = (-2.5,0)
+else:
+    move_speed_ship_L = (1.8,0)
+    move_speed_ship_R = (-1.8,0)
 
 move = False
 running = True
@@ -69,9 +102,9 @@ while running:
     keys = pygame.key.get_pressed()
     # make sure the user does not move the bank off the screen
     if keys[pygame.K_LEFT] and ship_rect.x > 0:
-        ship_rect = ship_rect.move(-2,0)
+        ship_rect = ship_rect.move(move_speed_ship_R)
     if keys[pygame.K_RIGHT] and ship_rect.x < 1020:
-        ship_rect = ship_rect.move(2,0)
+        ship_rect = ship_rect.move(move_speed_ship_L)
     if keys[pygame.K_SPACE]:
         if pew_pew.y < 0:
             pew_pew.y = ship_rect.centery
@@ -112,6 +145,11 @@ while running:
         exec(open("successful_escape.py").read())
     # detect collision between ship and cops
     if ship_rect.colliderect(cop1_rect) or  ship_rect.colliderect(cop2_rect) or ship_rect.colliderect(cop3_rect):
+        cop1_rect.center = (random.randint(50,300), 0)
+        cop2_rect.center = (random.randint(350,900), 0)
+        cop3_rect.center = (random.randint(950,1100), 0)
+        hearts_lost += 1 
+    if(hearts_lost == 2):
         # open the scene to explore town when the ship is exploded
         exec(open("exploded_ship.py").read())
     if pew_pew.colliderect(cop1_rect):
