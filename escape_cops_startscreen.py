51d29625619065a7a5b94d27e75e14d5e34aa3f9
Katja Murray <kmurra84@uwo.ca>
2023-04-04 14:15:58 -0400

    fixing scrapyard

--- escape_cops_startscreen.py
+++ escape_cops_startscreen.py
@@ -1,56 +1,33 @@
-import pygame, time
-import gameElements
-        
+import pygame
+
 pygame.init()
 
+# create the screen and its rect object
+screen = pygame.display.set_mode((1200, 768))
+screen_rect = screen.get_rect()
+pygame.display.set_caption("Escape Cops Startscreen")
+
+# load the background and create its rect
 bg_img = pygame.image.load('images/escape_start_background.png')
 bg_rect = bg_img.get_rect()
 
-screen = pygame.display.set_mode((1200, 768))
-screen_rect = screen.get_rect()
-pygame.display.set_caption("")
+# render function
+def render():
+    screen.blit(bg_img, bg_rect)
+    pygame.display.flip()
 
-startButton = gameElements.Button('images/Start.png',(300, 484),130,75,20,4, screen)
-# exitButton = gameElements.Button('images/Big_Exit.png',(900,484),130,75,20,4, screen)
-screen.blit(bg_img, bg_rect)
-
-Black = (0,0,0)
-White = (255,255,255)
-
-# #create text
-# game_status = ''
-# if gameElements.minigame_3_complete == False:
-#     game_status ='INCOMPLETE'
-# elif gameElements.minigame_3_complete == True:
-#     game_status = 'COMPLETE'
-
-# font = pygame.font.Font('PublicPixel-z84yD.ttf', 32)
-# text = font.render(game_status, True, White, Black)
-# textRect = text.get_rect()
-# textRect.center = (600,700)
-
-running = True
 # game loop
+running = True
 while running:
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            # if the space bar is pressed, move to next scene
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                exec(open('escape_cops_hard.py').read())
 
-    if startButton.command == True:
-        #if gameElements.minigame_1_complete == False: 
-            exec(open('escape_cops_hard.py').read())
-            startButton.command = False
-        #elif gameElements.minigame_1_complete == True:
-            #startButton.command = False
-    # elif exitButton.command == True:
-    #     exec(open('minigame3_scrapyard.py').read())
-    #     exitButton.command = False
+    render()
 
-    screen.blit(bg_img, bg_rect)
-    # screen.blit(text,textRect)
-    startButton.check_hover()
-    # exitButton.check_hover()
-    pygame.display.flip()
-    time.sleep(0.05)
 pygame.quit()
--- scrapyard_success.py
+++ scrapyard_success.py
@@ -1,43 +1,33 @@
-import pygame, time
-import gameElements
+import pygame
 
-        
 pygame.init()
 
+# create the screen and its rect object
+screen = pygame.display.set_mode((1200, 768))
+screen_rect = screen.get_rect()
+pygame.display.set_caption("Build Ship")
+
+# load the background and create its rect
 bg_img = pygame.image.load('images/scrapyard_success_background.png')
 bg_rect = bg_img.get_rect()
 
-screen = pygame.display.set_mode((1200, 768))
-screen_rect = screen.get_rect()
-pygame.display.set_caption("Upgrade Choice")
+# render function
+def render():
+    screen.blit(bg_img, bg_rect)
+    pygame.display.flip()
 
-shieldButton = gameElements.Button('images/Start.png',(300, 484),130,75,20,4, screen)
-speedButton = gameElements.Button('images/Big_Exit.png',(900,484),130,75,20,4, screen)
-screen.blit(bg_img, bg_rect)
-
-Black = (0,0,0)
-White = (255,255,255)
-
-running = True
 # game loop
+running = True
 while running:
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            # if the space bar is pressed, move to next scene
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                exec(open('escape_cops_startscreen.py').read())
 
-    if shieldButton.command == True:
-        gameElements.shield_power = True
-        exec(open('escape_cops_startscreen.py').read())
-        shieldButton.command = False
-    elif speedButton.command == True:
-        gameElements.speed_power = True
-        exec(open('escape_cops_startscreen.py').read())
-        speedButton.command = False
+    render()
 
-    screen.blit(bg_img, bg_rect)
-    shieldButton.check_hover()
-    speedButton.check_hover()
-    pygame.display.flip()
-    time.sleep(0.05)
 pygame.quit()
